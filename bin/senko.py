2fb9c66f409a8a7baf16254eb38acba12c0f2cfe
RangerDigital <kuba07071999@gmail.com>
2019-06-30 18:35:57 +0200

    Debug memory

--- bin/senko.py
+++ bin/senko.py
@@ -1,5 +1,6 @@
 import urequests
 import uhashlib
+import gc
 
 
 class Senko:
@@ -16,22 +17,27 @@ class Senko:
             print(msg)
 
     def _check_hash(self, x, y):
+        print(gc.mem_free())
+        gc.collect()
         x_hash = uhashlib.sha1(x.encode())
         y_hash = uhashlib.sha1(y.encode())
-
+        print(gc.mem_free())
         x = x_hash.digest()
         y = y_hash.digest()
-
+        print(gc.mem_free())
         if str(x) == str(y):
             return True
         else:
             return False
 
     def _get_file(self, url):
+        print(gc.mem_free())
+        gc.collect()
         payload = urequests.get(url, headers=self.headers)
         code = payload.status_code
 
         if code == 200:
+            print(gc.mem_free())
             return payload.text
         else:
             self._debug("Request returned error code, " + str(code))
@@ -42,22 +48,27 @@ class Senko:
 
         for file in self.files:
             latest_version = self._get_file(self.url + file)
+            print(gc.mem_free())
             if latest_version is None:
                 return []
 
+            print(gc.mem_free())
             try:
                 with open(file, "r") as local_file:
                     local_version = local_file.read()
             except:
                 local_version = ""
 
+            print(gc.mem_free())
             if not self._check_hash(latest_version, local_version):
                 changes.append(file)
 
         return changes
 
     def fetch(self):
+        print(gc.mem_free())
         if not self._check_all():
+            print(gc.mem_free())
             self._debug("No changes detected!")
             return False
         else:
