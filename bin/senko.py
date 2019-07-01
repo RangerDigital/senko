f034f916ce4103c6a7836c662ca4bd007f86261a
RangerDigital <kuba07071999@gmail.com>
2019-07-01 13:20:39 +0200

    Refactor, Clean-Up

--- bin/senko.py
+++ bin/senko.py
@@ -1,6 +1,5 @@
 import urequests
 import uhashlib
-import gc
 
 
 class Senko:
@@ -12,35 +11,25 @@ class Senko:
 
         self.files = files
 
-    def _debug(self, msg):
-        if self.debug:
-            print(msg)
-
     def _check_hash(self, x, y):
-        print(gc.mem_free())
-        gc.collect()
         x_hash = uhashlib.sha1(x.encode())
         y_hash = uhashlib.sha1(y.encode())
-        print(gc.mem_free())
+
         x = x_hash.digest()
         y = y_hash.digest()
-        print(gc.mem_free())
+
         if str(x) == str(y):
             return True
         else:
             return False
 
     def _get_file(self, url):
-        print(gc.mem_free())
-        gc.collect()
         payload = urequests.get(url, headers=self.headers)
         code = payload.status_code
 
         if code == 200:
-            print(gc.mem_free())
             return payload.text
         else:
-            self._debug("Request returned error code, " + str(code))
             return None
 
     def _check_all(self):
@@ -48,45 +37,34 @@ class Senko:
 
         for file in self.files:
             latest_version = self._get_file(self.url + file)
-            print(gc.mem_free())
             if latest_version is None:
                 return []
 
-            print(gc.mem_free())
             try:
                 with open(file, "r") as local_file:
                     local_version = local_file.read()
             except:
                 local_version = ""
 
-            print(gc.mem_free())
             if not self._check_hash(latest_version, local_version):
                 changes.append(file)
 
         return changes
 
     def fetch(self):
-        print(gc.mem_free())
         if not self._check_all():
-            print(gc.mem_free())
-            self._debug("No changes detected!")
             return False
         else:
-            self._debug("Changes detected!")
             return True
 
     def update(self):
-        self._debug("Updating...")
         changes = self._check_all()
 
         for file in changes:
-            self._debug("File: " + file)
             with open(file, "w") as local_file:
                 local_file.write(self._get_file(self.url + file))
 
         if changes:
-            self._debug("Done!")
             return True
         else:
-            self._debug("No changes were made!")
             return False
