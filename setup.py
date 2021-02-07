10bfd52adaa89b02f919abb890a86fbdf9266e6f
RangerDigital <36769448+RangerDigital@users.noreply.github.com>
2021-02-07 12:33:57 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="micropython-senko",
+    version="2.0.0",
+    author="Jakub Bednarski",
+    author_email="Jakub@Bednarski.dev",
+    description="Simplest OTA update solution for your Micropython projects.",
+    url="https://github.com/RangerDigital/senko",
+    packages=["senko"]
+)
