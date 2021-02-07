76574afd4cdd1ea3f5bc99df0dd94806cb422f7b
RangerDigital <36769448+RangerDigital@users.noreply.github.com>
2021-02-07 13:21:33 +0100

    Bump setup.py

---
+++ senko/__init__.py
@@ -0,0 +1,1 @@
+from .senko import Senko
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="micropython-senko",
-    version="2.0.0",
+    version="2.0.3",
     author="Jakub Bednarski",
     author_email="Jakub@Bednarski.dev",
     description="Simplest OTA update solution for your Micropython projects.",
